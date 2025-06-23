47b584706f737eae8c04038a40f3c5d38b782aab
Andrea Scala <55944438+andrea-scala@users.noreply.github.com>
2025-06-23 09:23:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ from telegram.ext import ApplicationBuilder
 from handlers.carica_scheda import carica_scheda_handler
 from handlers.allenati import allenati_conv
 from handlers.storico import storico_conv
-
-app = ApplicationBuilder().token("FAKE_TELEGRAM_TOKEN").build()
+import os
+app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 
 app.add_handler(carica_scheda_handler)
 app.add_handler(allenati_conv)
